19733acf0cd809fe046960c9345ef9ee27a0327c
Francisco Ibaceta <fibaceta.arce@gmail.com>
2025-09-11 13:05:23 +0000

    recuerda que debe ocupar OPENAI_API_KEY y hacer todos los metodos con ch

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,12 +1,8 @@
 import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
 import {openAI} from 'genkitx-openai';
 
 export const ai = genkit({
   plugins: [
-    googleAI({
-      apiKey: process.env.GEMINI_API_KEY,
-    }),
     openAI({
       apiKey: process.env.OPENAI_API_KEY,
     }),
